5fc3bdc2a17a3decfe63cad7f64a5c45171f347d
Dima Shamis <dims90995@gmail.com>
2025-06-04 19:59:50 +0300

    updates to mongo

--- MovieSearch/server/index.js
+++ MovieSearch/server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/movies', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
